a3473d78badddc34a7f965bb7aab53d49b416226
minseo300 <lmslmsms0616@gmail.com>
2021-04-30 14:41:34 +0900

    First Commit

---
+++ app/src/main/java/gachon/inclass/pcbangreservation/MainActivity.java
@@ -0,0 +1,18 @@
+package gachon.inclass.pcbangreservation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        finish();
+        startActivity(new Intent(this,LoginActivity.class));
+    }
+}
